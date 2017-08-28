165282d30a0be1c73fa8f8f33c0792f37c1c6344
AlbanyOregon <30977625+AlbanyOregon@users.noreply.github.com>
2017-08-28 14:17:30 -0700

    Create 1878LinnCountyAtlas.php

---
+++ 1878LinnCountyAtlas.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html><head><title>Historical Atlas Map Of Marion & Linn Counties Oregon. Compiled, Drawn And Published From Personal Examinations And Actual Surveys By Edgar Williams & Co. San Francisco, California. 1878.</title> 
+ <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="PUBLIC"/>
+<META NAME="COPYRIGHT" CONTENT="&copy; 2016 Joe LaBerge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta property="og:image"  content="https://www.davidrumsey.com/rumsey/Size0/D5005/5056003.jpg"/>
+ <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
+  <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
+    <meta property="fb:app_id" content="117812298943734" />  
+  <meta property="og:title"  content="Historical Atlas Map Of Marion & Linn Counties Oregon. Compiled, Drawn And Published From Personal Examinations And Actual Surveys By Edgar Williams & Co. San Francisco, California. 1878." /> 
+ <style>html, body {margin:0;padding:0;height:100%;-ms-content-zooming:none;}label {margin:0;padding:0;height:100%;display:block;overflow:auto;-ms-content-zooming:zoom;-ms-content-zoom-limit-max:500%;-ms-content-zoom-chaining:none;}img {display:block;max-width:100%;max-height:100%;}input {display:none;}input:checked + label > img {max-width:none;max-height:none;}input:checked + label {-ms-content-zoom-limit-min:20%;}</style><style media="(max-width: 2054px), (max-height: 1800px)">img {cursor:zoom-in;}input:checked + label > img {cursor:zoom-out;}
+ </style><style>
+ h2{
+  float: left;
+ }</style>
+</head><body style="background-color:black;"><h2><label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056002.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056003.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056004.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056005.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056006.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056007.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056008.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056009.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056010.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056011.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056012.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056013.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056014.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056015.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056016.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056017.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056018.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056019.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056020.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056021.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056022.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056023.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056024.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056025.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056026.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056027.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056028.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056029.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056030.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056031.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056032.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056033.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056034.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056035.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056036.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056037.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056038.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056039.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056040.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056041.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056042.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056043.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056044.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056045.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056046.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056047.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056048.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056049.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056050.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056051.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056052.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056053.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056054.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056055.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056056.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056057.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056058.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056059.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056060.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056061.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056062.jpg"></label>
+<label id="imgContainer" for="zoom"><img src="https://www.davidrumsey.com/rumsey/Size0/D5005/5056063.jpg"></label><div class="fb-like" data-href="https://albanyoregon.github.io/history/1878LinnCountyAtlas.php" data-width="225" data-layout="standard" data-action="like" data-colorscheme="dark" data-size="large" data-show-faces="true" data-share="true"></div><div class="fb-save" data-uri="https://albanyoregon.github.io/history/1878LinnCountyAtlas.php" data-size="large"></div><div class="fb-page" data-href="https://www.facebook.com/AlbanyOrDie" data-width="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/AlbanyOrDie" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AlbanyOrDie">Albany ᴴᶤᶳᵗᵒʳᶤᶲᵘᵉ</a></blockquote></div></h2>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '117812298943734',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();   
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script></body></html>
